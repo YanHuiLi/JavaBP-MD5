1f411fbd5ca0368cd84d7a7d29845265b439ca64
Archer <421228286@qq.com>
2016-06-05 20:34:51 +0800

    MD5 Java实现

--- src/MD5/MD5.java
+++ src/MD5/MD5.java
@@ -11,28 +11,28 @@ public class MD5 {
     public static void main(String[] args)  {
         String password="123456dsfgAASAS..-*/123";
 
-        try {
-            MessageDigest instance = MessageDigest.getInstance("MD5");
-            byte[] digest = instance.digest(password.getBytes());
+    try {
+        MessageDigest instance = MessageDigest.getInstance("MD5");
+        byte[] digest = instance.digest(password.getBytes());
 
-            StringBuilder sb=new StringBuilder();
-       for (byte b:digest){
-           int i=b&0xff;
-           String hexString = Integer.toHexString(i);
-           //转为16进制，获取低八位
+        StringBuilder sb=new StringBuilder();
+        for (byte b:digest){
+            int i=b&0xff;
+            String hexString = Integer.toHexString(i);
+            //转为16进制，获取低八位
 //           System.out.println(hexString);
-           if(hexString.length()<2){
+            if(hexString.length()<2){
 
-               hexString="0"+hexString;
+                hexString="0"+hexString;
 
-           }
-           sb.append(hexString);
-       }
-            System.out.println(sb.toString());
-            System.out.println(sb.toString().length());
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            }
+            sb.append(hexString);
         }
+        System.out.println(sb.toString());
+        System.out.println(sb.toString().length());
+    } catch (NoSuchAlgorithmException e) {
+        e.printStackTrace();
     }
+}
 
 }
